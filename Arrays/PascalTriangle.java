185b9a75e80d7d163d1bb17cb8a95f87895e81e7
Adnan Shahid <adnansh2804@gmail.com>
2023-07-17 14:33:27 +0530

    array 2/6

---
+++ Arrays/PascalTriangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> tri=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+                List<Integer> temp=new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    temp.add(1);
+                }
+                else{
+                    temp.add(tri.get(i-1).get(j)+tri.get(i-1).get(j-1));
+                }
+            }
+            tri.add(temp);
+        }
+        return tri;
+    }
+}
---
+++ Arrays/SetMatrixZero.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int col0=1;
+        //marking the initial row and column
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    if(j==0){
+                        col0=0;
+                    }else{
+                        matrix[0][j]=0;
+                    }
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        //checking the mark and altering the matrix
+        //changing row elements
+        for(int i=1;i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<matrix[i].length;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        //changing column elements
+        for(int i=1;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                for(int j=0;j<matrix.length;j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        //changing the first row and column
+        if(matrix[0][0]==0){
+            if(col0==0){
+                for(int i=0;i<matrix.length;i++){
+                    matrix[i][0]=0;
+                }
+            }
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==0){
+                for(int i=0;i<matrix.length;i++){
+                    matrix[i][0]=0;
+                }
+        }
+    }
+}
